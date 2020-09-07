60a583acaaf4f00f2ec02cc08aa1ad8848e8e186
arielfranciscobandtec <ariel.francisco@bandtec.com.br>
2020-09-06 21:16:21 -0300

    Criação Arquivos/ Init Spring

---
+++ grimorioFantasia/src/main/java/br/com/bandtec/grimorioFantasia/GrimorioFantasiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.grimorioFantasia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimorioFantasiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrimorioFantasiaApplication.class, args);
+	}
+
+}
---
+++ grimorioFantasia/src/test/java/br/com/bandtec/grimorioFantasia/GrimorioFantasiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.grimorioFantasia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrimorioFantasiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
